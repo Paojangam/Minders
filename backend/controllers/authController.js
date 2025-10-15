44a2a8eeafc384906a83806a9595c3bdb8274c67
Paojangam Kipgen <paojangam1234@gmail.com>
2025-10-15 10:40:48 +0530

    update auth

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -2,21 +2,17 @@ const User = require('../models/User');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
 
-// POST /auth/register
 const registerUser = async (req, res) => {
   const { firstName, lastName, email, password } = req.body;
 
   try {
-    // Check if user already exists
     const existingUser = await User.findOne({ email });
     if (existingUser) {
       return res.status(400).json({ message: 'User already exists' });
     }
 
-    // Hash password
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    // Create new user
     const newUser = new User({
       firstName,
       lastName,
@@ -26,12 +22,10 @@ const registerUser = async (req, res) => {
 
     await newUser.save();
 
-    // Generate JWT token
     const token = jwt.sign({ userId: newUser._id }, process.env.JWT_SECRET, {
       expiresIn: '1h',
     });
 
-    // Respond with token and user details
     res.status(201).json({
       token,
       user: {
@@ -49,29 +43,24 @@ const registerUser = async (req, res) => {
   }
 };
 
-// POST /auth/login
 const loginUser = async (req, res) => {
   const { email, password } = req.body;
 
   try {
-    // Find user by email
     const user = await User.findOne({ email });
     if (!user) {
       return res.status(400).json({ message: 'Invalid email or password' });
     }
 
-    // Compare password
     const isMatch = await bcrypt.compare(password, user.password);
     if (!isMatch) {
       return res.status(400).json({ message: 'Invalid email or password' });
     }
 
-    // Generate JWT token
     const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
       expiresIn: '3h',
     });
 
-    // Respond with token and user details
     res.status(200).json({
       token,
       user: {
